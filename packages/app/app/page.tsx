8f00ac3ba42213c1effcb49c0781b9366f16d31b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-22 14:38:08 +0900

    Update page.tsx

--- packages/app/app/page.tsx
+++ packages/app/app/page.tsx
@@ -4,7 +4,18 @@ import publicFile from "../utils/public-file"
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Image src={publicFile`/koujishin.svg`} alt="Logo" />
+      <div className="text-center>
+        <Image
+          src={publicFile`/koujishin.svg`}
+          alt="Logo"
+          className=""
+        />
+      </div>
+      <div className="mx-10">
+        <div>
+          広辞深は、オープンソースのWebをベースにした電子辞書です。
+        </div>
+      </div>
     </main>
   )
 }
