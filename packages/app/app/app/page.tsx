057c35fcaf50c7e46db37c5da5e5c5e11c7ed53f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-22 17:07:24 +0900

    Create page.tsx

---
+++ packages/app/app/app/page.tsx
@@ -0,0 +1,17 @@
+export default () => {
+  return (
+    <>
+      <div className="">
+        <div>
+          
+        </div>
+        <div>
+          <select>
+            <option value="yoko">ヨコ</option>
+            <option value="tate">タテ</option>
+          </select>
+        </div>
+      </div>
+    </>
+  )
+}
