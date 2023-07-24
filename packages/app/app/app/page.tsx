d83a696ee4b649b8dc2327743504dbc4ecdb0fd9
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-24 14:58:25 +0900

    デザイン

--- packages/app/app/app/page.tsx
+++ packages/app/app/app/page.tsx
@@ -5,11 +5,18 @@ export default () => {
         <div>
           
         </div>
-        <div className="fixed bottom-0 left-0">
-          <select>
-            <option value="yoko">ヨコ</option>
-            <option value="tate">タテ</option>
-          </select>
+        <div className="fixed bottom-0 left-0 bg-slate-50 dark:bg-slate-700 drop-shadow-lg rounded-full">
+          <div className="flex">
+            <div>
+              <span>広辞深 Alpha</span>
+            </div>
+            <div>
+              <select>
+                <option value="yoko">ヨコ</option>
+                <option value="tate">タテ</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
     </>
