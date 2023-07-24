b6bc1f6fc392e68723074f3748ac4b83b2cac19e
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-07-24 15:01:42 +0900

    Update page.tsx

--- packages/app/app/app/page.tsx
+++ packages/app/app/app/page.tsx
@@ -5,7 +5,7 @@ export default () => {
         <div>
           
         </div>
-        <div className="fixed bottom-0 left-0 bg-slate-50 dark:bg-slate-700 drop-shadow-lg rounded-full">
+        <div className="fixed bottom-0 left-0 bg-slate-50 dark:bg-slate-700 drop-shadow-lg rounded-full w-full p-4 m-4">
           <div className="flex">
             <div>
               <span>広辞深 Alpha</span>
